53519c88b1810337ada17894defb456d776a37c9
Kotetsu369 <christi-95@mail.ru>
2024-02-06 23:33:07 +0900

    Fixed search

--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
 import sqlite3
 
-connection = sqlite3.connect('students.db')
+connection = sqlite3.connect('courses_students.db')
 cursor = connection.cursor()
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS Students (
 id INTEGER PRIMARY KEY,
 name TEXT NOT NULL,
@@ -10,57 +11,57 @@ age INT,
 city TEXT NOT NULL)
 ''')
 connection.commit()
+
 #students = [('Max', 'Brooks', 24, 'Spb'), ('John', 'Stones', 15, 'Spb'), ('Andy', 'Wings', 45, 'Manhester'), ('Kate', 'Brooks', 34, 'Spb')]
 #cursor.executemany("INSERT INTO Students(name, surname, age, city) VALUES(?, ?, ?, ?)", students)
-
 #connection.commit()
-connection.close()
 
-connection = sqlite3.connect('courses.db')
-cursor = connection.cursor()
+
 cursor.execute('''CREATE TABLE IF NOT EXISTS Courses (
 id INTEGER PRIMARY KEY,
 name TEXT NOT NULL,
 time_start DATE,
 time_end DATE)
 ''')
-
 connection.commit()
+
 #courses = [(1, 'python', '21.07.21', '21.08.21'),
 #           (2, 'java', '13.07.21', '16.08.21')]
 #cursor.executemany("INSERT INTO courses VALUES(?, ?, ?, ?)", courses)
 #connection.commit()
-connection.close()
-
-connection = sqlite3.connect('Student_courses.db')
-cursor = connection.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS Student_courses (
 courses_id INT REFERENCES Students(id),
 student_id INT REFERENCES Courses(id))
 ''')
+connection.commit()
 
-#connection.commit()
 #Student_courses = [(1, 1), (2, 1), (3, 1), (4, 2)]
 #cursor.executemany("INSERT INTO Student_courses(student_id,courses_id) VALUES(?, ?)", Student_courses)
-
-connection.commit()
-connection.close()
+#connection.commit()
 
 #Вссе студенты,кто старше 30
-connection = sqlite3.connect('students.db')
-cursor = connection.cursor()
 students_30 = cursor.execute("SELECT name, age FROM Students WHERE age > 30").fetchall()
 print(students_30)
 
-#connection.close()
-
 #Всех студентов, которые проходят курс по python.
 
-connection = sqlite3.connect('Student_courses.db')
-cursor = connection.cursor()
-students_python = cursor.execute('''SELECT Students_name
-    FROM Student_Courses
-    WHERE (Courses_id = 1) AND
-    (Student_id=Student_Courses.student_id)''').fetchall()
+students_python = cursor.execute('''SELECT Students.name, Courses.name
+    FROM Students
+    JOIN  Student_courses
+    ON (Student_courses.student_id = Students.id)
+    JOIN Courses
+    ON (Courses.id = Student_courses.courses_id) AND
+    (Student_courses.courses_id=1)''').fetchall()
 print(students_python)
+
+# Всех студентов, которые проходят курс по python и из Spb
+
+python_SPB = cursor.execute('''SELECT Students.name, Courses.name, Students.city
+    FROM Students, Courses, Student_courses
+    WHERE (Student_courses.student_id = Students.id) AND (Students.city='Spb')
+    AND (Courses.id = Student_courses.courses_id) AND
+    (Student_courses.courses_id=1)''').fetchall()
+print(python_SPB)
+
+connection.close()
